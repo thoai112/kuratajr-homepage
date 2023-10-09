7ebf70ed935e0a7ace2eaedec98c8ab8c78d0e9d
Kuratajr <thoailata1000@gmail.com>
2023-10-09 15:20:40 +0700

    up

--- pages/index.js
+++ pages/index.js
@@ -124,15 +124,15 @@ const Home = () => (
         <BioSection>
           <BioYear>2010</BioYear>
           Completed the Master&apos;s Program in the Graduate School of
-          Information Science at Nara Institute of Science and Technology
-          (奈良先端科学技術大学院大学情報科学研究科修士課程)
+          Posts and Telecommunications Institute of Technology
+          (PTIT)
         </BioSection>
         <BioSection>
-          <BioYear>2010</BioYear>
-          Worked at Yahoo! Japan (ヤフー株式会社入社)
+          <BioYear>2021</BioYear>
+          Worked at FPT (System Engineer)
         </BioSection>
         <BioSection>
-          <BioYear>2012 to present</BioYear>
+          <BioYear>2022 to present</BioYear>
           Working as a freelancer
         </BioSection>
       </Section>
