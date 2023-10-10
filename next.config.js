6bde9bf4307bc0885c91754547531059749c25f3
Kuratajr <thoailata1000@gmail.com>
2023-10-10 20:58:19 +0700

    up

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
+      },
+    ]
+  },
     reactStrictMode: true,
     swcMinify: true
   }
