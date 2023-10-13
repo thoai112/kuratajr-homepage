5088b6bee9aea2a9fe26fad1f2a423ee775de2e3
Kuratajr <thoailata1000@gmail.com>
2023-10-13 16:01:50 +0700

    up

--- pages/works.js
+++ pages/works.js
@@ -3,6 +3,8 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 
+import thumbApacheAirflow from '../public/images/works/apacheairflow_eyecatch.png'
+
 import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
 import thumbWalknote from '../public/images/works/walknote_eyecatch.png'
 import thumbFourPainters from '../public/images/works/the-four-painters_eyecatch.jpg'
@@ -23,11 +25,15 @@ const Works = () => (
 
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section>
-          <WorkGridItem id="inkdrop" title="Inkdrop" thumbnail={thumbInkdrop}>
-            A Markdown note-taking app with 100+ plugins, cross-platform and
-            encrypted data sync support
+          <WorkGridItem 
+            id="apacheairflow" 
+            title="apacheairflow" 
+            thumbnail={thumbApacheAirflow}
+          >
+            Building Business intelligence datapipeline using Apache Airflow
           </WorkGridItem>
         </Section>
+
         <Section>
           <WorkGridItem
             id="walknote"
