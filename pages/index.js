f8962d1e54916a1a613175c09f0440b2bc5b35b7
Kuratajr <thoailata1000@gmail.com>
2023-10-13 14:10:56 +0700

    up

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ const Home = () => (
 
       <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
+          <Heading as="h2" variant="page-title" fontSize={30}>
             Xuân Thoại( Kuratajr )
           </Heading>
           <p>Digital Craftsman ( Devops / Developer )</p>
