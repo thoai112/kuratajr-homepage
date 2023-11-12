1e3e873650b5f462020220c209d0f883121bf758
thoai112 <40832268+thoai112@users.noreply.github.com>
2023-11-12 10:14:43 +0700

    Update

--- pages/works/apacheairflow.js
+++ pages/works/apacheairflow.js
@@ -35,26 +35,26 @@ import {
             <Meta>Stack</Meta>
             <span>NodeJS, Electron, React Native</span>
           </ListItem>
-          <ListItem>
-            <Meta>Blogpost</Meta>
-            <Link href="https://blog.inkdrop.app/how-ive-attracted-the-first-500-paid-users-for-my-saas-that-costs-5-mo-7a5b94b8e820">
-              How I’ve Attracted The First 500 Paid Users For My SaaS That Costs
-              $5/mo <ExternalLinkIcon mx="2px" />
-            </Link>
-          </ListItem>
+          // <ListItem>
+          //   <Meta>Blogpost</Meta>
+          //   <Link href="https://blog.inkdrop.app/how-ive-attracted-the-first-500-paid-users-for-my-saas-that-costs-5-mo-7a5b94b8e820">
+          //     How I’ve Attracted The First 500 Paid Users For My SaaS That Costs
+          //     $5/mo <ExternalLinkIcon mx="2px" />
+          //   </Link>
+          // </ListItem>
         </List>
   
-        <WorkImage src="/images/works/inkdrop_01.png" alt="Inkdrop" />
-        <WorkImage src="/images/works/inkdrop_02.png" alt="Inkdrop" />
-        <AspectRatio maxW="640px" ratio={1.7} my={4}>
-          <iframe
-            src="https://www.youtube.com/embed/-qBavwqc_mY"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </AspectRatio>
+        // <WorkImage src="/images/works/inkdrop_01.png" alt="Inkdrop" />
+        // <WorkImage src="/images/works/inkdrop_02.png" alt="Inkdrop" />
+        // <AspectRatio maxW="640px" ratio={1.7} my={4}>
+        //   <iframe
+        //     src="https://www.youtube.com/embed/-qBavwqc_mY"
+        //     title="YouTube video player"
+        //     frameBorder="0"
+        //     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        //     allowFullScreen
+        //   ></iframe>
+        // </AspectRatio>
       </Container>
     </Layout>
   )
